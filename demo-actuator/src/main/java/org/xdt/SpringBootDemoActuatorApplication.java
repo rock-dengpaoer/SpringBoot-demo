cede9d87c8512ee8f28bc4cabdddcf2ec8dac161
rock_dengpaoer <xdt0916@qq.com>
2023-03-15 15:43:18 +0800

    主要演示了如何在 Spring Boot 中通过 actuator 检查项目运行情况

---
+++ demo-actuator/src/main/java/org/xdt/SpringBootDemoActuatorApplication.java
@@ -0,0 +1,17 @@
+package org.xdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class SpringBootDemoActuatorApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(SpringBootDemoActuatorApplication.class, args);
+    }
+}
